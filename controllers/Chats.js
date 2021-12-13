e04459824dab18ca7c6714974eacc6cf850c253a
tranngocphien <tranngocphien0309@gmail.com>
2021-12-13 09:11:34 +0700

    update

--- controllers/Chats.js
+++ controllers/Chats.js
@@ -1,100 +1,120 @@
-const {
-    PRIVATE_CHAT,
-    GROUP_CHAT,
-} = require('../constants/constants');
+const { PRIVATE_CHAT, GROUP_CHAT } = require("../constants/constants");
 const ChatModel = require("../models/Chats");
 const MessagesModel = require("../models/Messages");
 const httpStatus = require("../utils/httpStatus");
 const chatController = {};
 chatController.send = async (req, res, next) => {
-    try {
-        let userId = req.userId;
-        const {
-            name,
-            chatId,
-            receivedId,
-            member,
-            type,
-            content
-        } = req.body;
-        let chatIdSend = null;
-        let chat;
-        if (type === PRIVATE_CHAT) {
-            if (chatId) {
-                chat = await ChatModel.findById(chatId);
-                if (chat !== null) {
-                    chatIdSend = chat._id;
-                }
-            } else {
-                chat = new ChatModel({
-                   type: PRIVATE_CHAT,
-                   member: [
-                       receivedId,
-                       userId
-                   ]
-                });
-                await chat.save();
-                chatIdSend = chat._id;
-            }
-        } else if (type === GROUP_CHAT) {
-            if (chatId) {
-                chat = await ChatModel.findById(chatId);
-                if (chat !== null) {
-                    chatIdSend = chat._id;
-                }
-            } else {
-                chat = new ChatModel({
-                    type: GROUP_CHAT,
-                    member: member
-                });
-                await chat.save();
-                chatIdSend = chat._id;
-            }
+  try {
+    let userId = req.userId;
+    const { name, chatId, receivedId, member, type, content } = req.body;
+    let chatIdSend = null;
+    let chat;
+    if (type === PRIVATE_CHAT) {
+      if (chatId) {
+        chat = await ChatModel.findById(chatId);
+        if (chat !== null) {
+          chatIdSend = chat._id;
         }
-        if (chatIdSend) {
-            if (content) {
-                let message = new MessagesModel({
-                    chat: chatIdSend,
-                    user: userId,
-                    content: content
-                });
-                await message.save();
-                let messageNew = await MessagesModel.findById(message._id).populate('chat').populate('user');
-                return res.status(httpStatus.OK).json({
-                    data: messageNew
-                });
-            } else {
-                return res.status(httpStatus.OK).json({
-                    data: chat,
-                    message: 'Create chat success',
-                    response: 'CREATE_CHAT_SUCCESS'
-                });
-            }
-        } else {
-            return res.status(httpStatus.BAD_REQUEST).json({
-                message: 'Not chat'
-            });
-        }
-
-    } catch (e) {
-        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            message: e.message
+      } else {
+        chat = new ChatModel({
+          type: PRIVATE_CHAT,
+          member: [receivedId, userId],
         });
+        await chat.save();
+        chatIdSend = chat._id;
+      }
+    } else if (type === GROUP_CHAT) {
+      if (chatId) {
+        chat = await ChatModel.findById(chatId);
+        if (chat !== null) {
+          chatIdSend = chat._id;
+        }
+      } else {
+        chat = new ChatModel({
+          type: GROUP_CHAT,
+          member: member,
+        });
+        await chat.save();
+        chatIdSend = chat._id;
+      }
     }
-}
+    if (chatIdSend) {
+      if (content) {
+        let message = new MessagesModel({
+          chat: chatIdSend,
+          user: userId,
+          content: content,
+        });
+        await message.save();
+        let messageNew = await MessagesModel.findById(message._id)
+          .populate("chat")
+          .populate("user");
+        return res.status(httpStatus.OK).json({
+          data: messageNew,
+        });
+      } else {
+        return res.status(httpStatus.OK).json({
+          data: chat,
+          message: "Create chat success",
+          response: "CREATE_CHAT_SUCCESS",
+        });
+      }
+    } else {
+      return res.status(httpStatus.BAD_REQUEST).json({
+        message: "Not chat",
+      });
+    }
+  } catch (e) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: e.message,
+    });
+  }
+};
 chatController.getMessages = async (req, res, next) => {
-    try {
-        let messages = await MessagesModel.find({
-            chat: req.params.chatId
-        }).populate('user');
-        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            data: messages
-        });
-    } catch (e) {
-        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            message: e.message
-        });
+  try {
+    let messages = await MessagesModel.find({
+      chat: req.params.chatId,
+    }).populate("user");
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      data: messages,
+    });
+  } catch (e) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: e.message,
+    });
+  }
+};
+
+chatController.getMessaged = async (req, res, next) => {
+  let userId = req.userId;
+  let listChats = await ChatModel.find({ member: { $all: [userId] } });
+  let listMessages = await MessagesModel.find({ chat: { $in: listChats } })
+    .populate("chat")
+    .populate("user");
+
+  let map = new Map();
+  for (let message of listMessages) {
+    let key = message.chat.toString();
+
+    if (map.has(key)) {
+      if (map.get(key).updatedAt < message.updatedAt) {
+        map.set(key, message);
+      }
+    } else {
+      map.set(key, message);
     }
-}
+  }
+
+  let result = [];
+  for (let [key, value] of map) {
+    result.push(value);
+  }
+
+  return res.status(httpStatus.OK).json({
+    data: result,
+    message: "Get list success",
+    response: "GET LIST SUCCESS",
+  });
+};
 
 module.exports = chatController;
--- routes/Chats.js
+++ routes/Chats.js
@@ -1,20 +1,17 @@
 const chatController = require("../controllers/Chats");
-const {asyncWrapper} = require("../utils/asyncWrapper");
+const { asyncWrapper } = require("../utils/asyncWrapper");
 const express = require("express");
 const chatsRoutes = express.Router();
 const auth = require("../middlewares/auth");
 
-chatsRoutes.post(
-    "/send",
-    auth,
-    asyncWrapper(chatController.send),
-);
+chatsRoutes.post("/send", auth, asyncWrapper(chatController.send));
 
 chatsRoutes.get(
-    "/getMessages/:chatId",
-    auth,
-    asyncWrapper(chatController.getMessages),
+  "/getMessages/:chatId",
+  auth,
+  asyncWrapper(chatController.getMessages)
 );
 
+chatsRoutes.get("/getMessaged", auth, asyncWrapper(chatController.getMessaged));
 
 module.exports = chatsRoutes;
